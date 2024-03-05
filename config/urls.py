2183fcc58ae8084686e0230b5651022cb86c2e00
Fabril <adrianfabril@gmail.com>
2024-03-05 08:03:20 -0500

    basedecarnet

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-"""proyecto_carnet URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from core.homepage.views import indexView
+from core.login.views import loginFormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('erp/', include('core.erp.urls')),
+    path('login/', include('core.login.urls'))
 ]
